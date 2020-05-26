90b45490de6290716954772059d9c8b10e71b26e
AdrianLopez12 <33838397+AdrianLopez12@users.noreply.github.com>
2020-05-26 17:30:01 -0500

    inicialisacion del proyecto

--- Practica-de-laboratorio-03-AdrianLopez/src/ec/edu/ups/interfaz/Pantalla.java
+++ Practica-de-laboratorio-03-AdrianLopez/src/ec/edu/ups/interfaz/Pantalla.java
@@ -7,11 +7,24 @@ import java.awt.Font;
 import java.awt.Panel;
 import java.awt.Label;
 import java.awt.GridLayout;
+import java.awt.TextField;
 
 public class Pantalla {
+Frame pantallaInicio = new Frame();
+Frame pantallaRegistroT = new Frame();
+Frame pantallaEliminarT = new Frame();
+Frame pantallaListarT = new Frame();
+Frame pantallaModificar = new Frame();
+
 
     public void mostrarventana1() {
-        Frame pantallaInicio = new Frame();
+        pantallaEliminarT.setVisible(false);
+        pantallaListarT.setVisible(false);
+        pantallaRegistroT.setVisible(false);
+        pantallaModificar.setVisible(false);
+        pantallaInicio.removeAll();
+        pantallaInicio.setVisible(false);
+        
         pantallaInicio.setSize(600, 400);
 
         pantallaInicio.setLocationRelativeTo(null);
@@ -57,6 +70,188 @@ pantallaInicio.add(label1);
 pantallaInicio.setVisible(true);
 
     }
+public void RegistroT(){
+    
+    pantallaInicio.setVisible(false);
+    pantallaRegistroT.removeAll();
+  
+    
+    Panel p1=new Panel();
+    Label inicio=new Label("Registro telefono");
+    pantallaRegistroT.setSize(600, 600);
+    inicio.setFont(new Font("Serif", Font.BOLD, 22));
+    
+    
+    GridLayout layaut1=new GridLayout(7, 0);
+    pantallaRegistroT.setLayout(layaut1);
+    pantallaRegistroT.add(p1);
+    p1.add(inicio);
+    TextField entrada1=new TextField();
+    Panel p2=new Panel();
+    
+    Label ntele=new Label("Ingrese numero de telefonos a registrar-->");
+    p2.add(ntele);
+    p2.add(entrada1);
+    pantallaRegistroT.add(p2);
+    
+    
+    Panel p3=new Panel();
+    Label cod=new Label("Ingrese codigo de telefonos-->");
+    TextField entrada2=new TextField();
+    p3.add(cod);
+    p3.add(entrada2);
+    pantallaRegistroT.add(p3);
+    
+    Panel p4=new Panel();
+    Label num=new Label("Ingrese numero de telefono-->");
+    TextField entrada3=new TextField();
+    p4.add(num);
+    p4.add(entrada3);
+    pantallaRegistroT.add(p4);
+    
+    Panel p5=new Panel();
+    Label tipo=new Label("Ingrese tipo de telefonos-->");
+    TextField entrada4=new TextField();
+    p5.add(tipo);
+    p5.add(entrada4);
+    pantallaRegistroT.add(p5);
+    
+    Panel p6=new Panel();
+    Label operadora=new Label("Ingrese operadora de telefonos-->");
+    TextField entrada5=new TextField();
+    p6.add(operadora);
+    p6.add(entrada5);
+    pantallaRegistroT.add(p6);
+    
+    Panel P7=new Panel();
+    Button B = new Button("Aceptar");
+    P7.add(B);
+    pantallaRegistroT.add(P7);
+    
+    pantallaRegistroT.setVisible(true);
+}
 
+public void modificar(){
+    pantallaInicio.setVisible(false);
+    pantallaModificar.removeAll();
+  
+    
+    Panel p1=new Panel();
+    Label inicio=new Label("MODIFICAR TELEFONO");
+    pantallaModificar.setSize(600, 600);
+    inicio.setFont(new Font("Serif", Font.BOLD, 22));
+    
+    
+    GridLayout layaut1=new GridLayout(7, 0);
+    pantallaModificar.setLayout(layaut1);
+    pantallaModificar.add(p1);
+    p1.add(inicio);
+    TextField entrada1=new TextField();
+    
+    
+  
+    
+    Panel p3=new Panel();
+    Label cod=new Label("Ingrese codigo de telefono a modificar-->");
+    TextField entrada2=new TextField();
+    p3.add(cod);
+    p3.add(entrada2);
+    pantallaModificar.add(p3);
+    
+    Panel p4=new Panel();
+    Label num=new Label("Ingrese nuevo numero -->");
+    TextField entrada3=new TextField();
+    p4.add(num);
+    p4.add(entrada3);
+    pantallaModificar.add(p4);
+    
+    Panel p5=new Panel();
+    Label tipo=new Label("Ingrese nuevo tipo-->");
+    TextField entrada4=new TextField();
+    p5.add(tipo);
+    p5.add(entrada4);
+    pantallaModificar.add(p5);
+    
+    Panel p6=new Panel();
+    Label operadora=new Label("Ingrese nueva operadora-->");
+    TextField entrada5=new TextField();
+    p6.add(operadora);
+    p6.add(entrada5);
+    pantallaModificar.add(p6);
+    pantallaModificar.setVisible(true);
+    
+    Panel P7=new Panel();
+    Button B = new Button("Aceptar");
+    P7.add(B);
+    pantallaModificar.add(P7);
+    
+}
+
+public void eliminarT(){
+    
+     pantallaInicio.setVisible(false);
+    pantallaEliminarT.removeAll();
+  
+    
+    Panel p1=new Panel();
+    Label inicio=new Label("ELIMINAR TELEFONO");
+    pantallaEliminarT.setSize(600, 300);
+    inicio.setFont(new Font("Serif", Font.BOLD, 22));
+    
+    
+    GridLayout layaut1=new GridLayout(3, 0);
+    pantallaEliminarT.setLayout(layaut1);
+    pantallaEliminarT.add(p1);
+    p1.add(inicio);
+    TextField entrada1=new TextField();
+    
+    
+  
+    
+    Panel p3=new Panel();
+    Label cod=new Label("Ingrese codigo de telefono a eliminar-->");
+    TextField entrada2=new TextField();
+    p3.add(cod);
+    p3.add(entrada2);
+    pantallaEliminarT.add(p3);
+    
+    Panel P7=new Panel();
+    Button B = new Button("Eliminar");
+    P7.add(B);
+    pantallaEliminarT.add(P7);
+    
+    pantallaEliminarT.setVisible(true);
+    
+}
+
+public void listarT(){
+    pantallaInicio.setVisible(false);
+    pantallaListarT.removeAll();
+  
+    
+    Panel p1=new Panel();
+    Label inicio=new Label("LISTA DE SUS TELEFONOS");
+    pantallaListarT.setSize(600, 300);
+    inicio.setFont(new Font("Serif", Font.BOLD, 22));
+    
+    GridLayout layaut1=new GridLayout(3, 0);
+    pantallaListarT.setLayout(layaut1);
+    pantallaListarT.add(p1);
+    p1.add(inicio);
+    
+    Label lista=new Label();
+    Panel p2=new Panel();
+    p2.add(lista);
+    
+    
+    Panel P7=new Panel();
+    Button B = new Button("Eliminar");
+    P7.add(B);
+    pantallaListarT.add(P7);
+    
+    pantallaListarT.setVisible(true);
+    
+    
+}
     
 }
--- Practica-de-laboratorio-03-AdrianLopez/src/ec/edu/ups/principal/Principal.java
+++ Practica-de-laboratorio-03-AdrianLopez/src/ec/edu/ups/principal/Principal.java
@@ -18,86 +18,93 @@ public class Principal {
     static byte op;
     static Scanner leer = new Scanner(System.in);
     List<Usuario> usuario = new ArrayList<>();
+     static Pantalla p =new Pantalla();
 
     public static void main(String[] args) {
         
-        Pantalla p =new Pantalla();
-        p.mostrarventana1();
+       
+       
         
         
-//        //Daos
-//        ImpleDao clienteDao = new ImpleDao();
-//
-////controladores
-//        ControladorUsuario contu = new ControladorUsuario();
-//        ControladorTelefono contT = new ControladorTelefono();
-//
-//        do {
-//            System.out.println("1) Registrarse");
-//            System.out.println("2) Iniciar secion");
-//            System.out.println("3) Mostrar numeros de telefono por cedula");
-//            System.out.println("4) Listar telefonos");
-//            System.out.println("5) Salir");
-//            op = leer.nextByte();
-//            if (op == 1) {
-//                Usuario usu1;
-//                usu1 = contu.CrearUsuario();
-//                clienteDao.GuardarUsuario(usu1);
-//
-//            }
-//            if (op == 2) {
-//
-//                System.out.println("ingrese correo");
-//                String correo = leer.next();
-//                System.out.println("Ingrese contrasena");
-//                String contrasena1 = leer.next();
-//                Usuario r = clienteDao.validarUsuario(correo, contrasena1);
-//                if (r != null) {
-//                    do {
-//                        System.out.println("Usuario valido");
-//                        System.out.println("1) Registrar telefono");
-//                        System.out.println("2) modificar");
-//                        System.out.println("3) Eliminar");
-//                        System.out.println("4) listar sus telefonos");
-//                        System.out.println("5) Regresar logueo");
-//                        op = leer.nextByte();
-//                        if (op == 1) {
-//                            List<Telefono> telf;
-//                            telf = contT.CrearTelefono();
-//                            clienteDao.actualizarTelfUsuario(r, telf);
-//
-//                        }
-//                        if (op == 2) {
-//
-//                            List<Telefono> t = r.getTelefono();
-//                            contT.ModificarTelefono(t);
-//
-//                        }
-//                        if (op == 3) {
-//                            Telefono te;
-//                            te = contT.BuscarTelefono(r.getTelefono());
-//                            clienteDao.EliminarTelefono(te, r);
-//
-//                        }
-//                        if (op == 4) {
-//
-//                            clienteDao.mostrarTelefono(r);
-//                        }
-//                        if (op == 5) {
-//                            op = 6;
-//
-//                        }
-//                    } while (op != 6);
-//                }
-//
-//            }
-//            if (op == 3) {
-//                contu.ListarTelefonosPorCedula();
-//            }
-//            if (op == 4) {
-//                clienteDao.mostrarTelefonos();
-//            }
-//
-//        } while (op != 5);
+        //Daos
+        ImpleDao clienteDao = new ImpleDao();
+
+//controladores
+        ControladorUsuario contu = new ControladorUsuario();
+        ControladorTelefono contT = new ControladorTelefono();
+
+        do {
+            System.out.println("1) Registrarse");
+            System.out.println("2) Iniciar secion");
+            System.out.println("3) Mostrar numeros de telefono por cedula");
+            System.out.println("4) Listar telefonos");
+            System.out.println("5) Salir");
+            op = leer.nextByte();
+            if (op == 1) {
+                Usuario usu1;
+                usu1 = contu.CrearUsuario();
+                clienteDao.GuardarUsuario(usu1);
+
+            }
+            if (op == 2) {
+
+                System.out.println("ingrese correo");
+                String correo = leer.next();
+                System.out.println("Ingrese contrasena");
+                String contrasena1 = leer.next();
+                Usuario r = clienteDao.validarUsuario(correo, contrasena1);
+                if (r != null) {
+                    do {
+                         p.mostrarventana1();
+                        System.out.println("Usuario valido");
+                        System.out.println("1) Registrar telefono");
+                        System.out.println("2) modificar");
+                        System.out.println("3) Eliminar");
+                        System.out.println("4) listar sus telefonos");
+                        System.out.println("5) Regresar logueo");
+                        op = leer.nextByte();
+                        if (op == 1) {
+                            p.RegistroT();
+                            List<Telefono> telf;
+                            telf = contT.CrearTelefono();
+                            clienteDao.actualizarTelfUsuario(r, telf);
+                            
+
+                        }
+                        if (op == 2) {
+                            p.modificar();
+
+                            List<Telefono> t = r.getTelefono();
+                            contT.ModificarTelefono(t);
+
+                        }
+                        if (op == 3) {
+                            p.eliminarT();
+                            Telefono te;
+                            te = contT.BuscarTelefono(r.getTelefono());
+                            clienteDao.EliminarTelefono(te, r);
+
+                        }
+                        if (op == 4) {
+                               
+                            clienteDao.mostrarTelefono(r);
+                            p.listarT();
+                        }
+                        if (op == 5) {
+                            op = 6;
+
+                        }
+                    } while (op != 6);
+                }
+
+            }
+            if (op == 3) {
+                contu.ListarTelefonosPorCedula();
+            }
+            if (op == 4) {
+                clienteDao.mostrarTelefonos();
+            }
+
+        } while (op != 5);
     }
 }
